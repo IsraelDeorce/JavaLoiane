3e6b99122ddb1b67f29b54c95e72058639fbc506
IsraelDeorce <israel.idvj@gmail.com>
2017-10-24 18:44:41 -0200

    Aula 67 finalizada

--- JavaLoiane/src/aula67/MinhaThread.java
+++ JavaLoiane/src/aula67/MinhaThread.java
@@ -1,20 +1,33 @@
 package aula67;
 
-//eu já tenho uma thread por estar extendendo uma
-public class MinhaThread extends Thread{
-	
-	//so para ter alguma coisa
+//eu jah tenho uma thread por estar extendendo uma
+public class MinhaThread extends Thread {
+
+	// so para ter alguma coisa
 	private String nome;
-	
-	public MinhaThread(String nome){
+	private int tempo;
+
+	public MinhaThread(String nome, int tempo) {
 		this.nome = nome;
+		this.tempo = tempo;
 		this.start();
 	}
-	
-	//aki vai fazê uqui a gente tem qi fazê
-	//o run é chamado pelo start()
-	public void run(){
-		System.out.println("Executando a Thread");				
+
+	// aki vai fazer o que a gente tem que fazer
+	// o run eh chamado pelo start()
+	public void run() {
+		try {
+			for (int i = 0; i < 6; i++) {
+				System.out.println(nome + " contador " + i);
+				//Esse metodo faz dormir por algum tempo, logo ele imprime 1 segundo cada vez
+				Thread.sleep(tempo);
+			}
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		System.out.println(nome + " terminou a execucao");
+
 	}
 
 }
--- JavaLoiane/src/aula67/Teste.java
+++ JavaLoiane/src/aula67/Teste.java
@@ -3,9 +3,11 @@ package aula67;
 public class Teste {
 
 	public static void main(String[] args) {
-		MinhaThread thread = new MinhaThread("Thread #1");
+		MinhaThread thread = new MinhaThread("Thread #1", 600);
 		//o start chama o run da thread. Colocamos ele no construtor
 		//thread.start();
+		
+		MinhaThread thread2 = new MinhaThread("Thread #2", 1000);	
 	}
 
 }
